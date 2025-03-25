a242acb06a006115dbb301912be2f5e98d84b3ce
gigrove <147515425+gigrove@users.noreply.github.com>
2025-03-25 14:24:55 +0100

    Made the ShowCall mechanic work! (singleplayer)
    now I just need to figure out how to code & test it so it works with two
    also the visual asset is still a janky placeholder

--- Assets/Scripts/ShowCallRenderer.cs
+++ Assets/Scripts/ShowCallRenderer.cs
@@ -1,17 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Video;
 
 public class ShowCallRenderer : MonoBehaviour
 {
     public Transform OtherPlayerPos;
-    public RectTransform Indicator;
+    public RectTransform GUIIndicator;
     public TMP_Text debugText;
+    public float pulseLength = 3;
+    public float fadeSpeed = 1;
 
-    private Vector3 renderPosition;
-    private bool behindCamera;
-    private Vector2 distanceToMidpoint;
+    private Vector3 rawScreenPosition;
+    private Vector3 finalScreenPosition;
 
     void Start()
     {
@@ -19,71 +23,92 @@ public class ShowCallRenderer : MonoBehaviour
         {
             Debug.LogWarning("You forgot to assign the OtherPlayer transform");
         }
+
+        GUIIndicator.localScale = Vector3.zero;
+
     }
 
     void Update()
     {
-
-        renderPosition = Camera.main.WorldToScreenPoint(OtherPlayerPos.position); //get worldPosition of other player into a screen space format
+        //COORDINATE UPDATING (constant)
+        rawScreenPosition = Camera.main.WorldToScreenPoint(OtherPlayerPos.position); //get worldPosition of other player into a screen space format
         //Debug.Log(renderPosition);
 
-        //clamp it to the screen size so it's always visible
-        renderPosition.x = Mathf.Clamp(renderPosition.x, 0, Screen.width); 
-        renderPosition.y = Mathf.Clamp(renderPosition.y, 0, Screen.height);
-
-
-        if (renderPosition.z < 0) //how do we display The Thing when its /behind/ the camera?
+        if (rawScreenPosition.z >= 0) //if in front of camera 
         {
-            behindCamera = true;
           
-            /*
-            //maybe push it to the edges of the screen at all times depending on its placement (so you know which way to turn)
-            if (renderPosition.x > (Screen.width / 2)) //if it's on the right side push it all the way right
-            {
-                renderPosition.x = Screen.width;
-            } else
-            {
-                renderPosition.x = 0;
-            }
-
-            if (renderPosition.y > (Screen.height / 2)) //if it's on the upper half push it all the way up
-            {
-                renderPosition.y = Screen.height;
-            } else //and vice versa
-            {
-                renderPosition.y = 0;
-            }
-            */
-
-            //imperfect – if behind, it'll /always/ be jammed into one of four corners. not ideal? not super wrong either
-            
-            /*
-            //next attempt: only push one side.
-            distanceToMidpoint.Set(renderPosition.x - (Screen.width/2), renderPosition.y - (Screen.height/2));
-            if (distanceToMidpoint.x > distanceToMidpoint.y) //if it's further away on the horizontal axis?? maybe?? then we push that side and just clamp y
-            {
-                if (renderPosition.x > (Screen.width / 2)) //if it's on the right side push it all the way right
-                {
-                    renderPosition.x = Screen.width;
-                } 
-                else
-                {
-                    renderPosition.x = 0;
-                }
-            }
-            */
+            //simply clamp it to the screen size so it's always visible
+            finalScreenPosition.x = Mathf.Clamp(rawScreenPosition.x, 0, Screen.width); 
+            finalScreenPosition.y = Mathf.Clamp(rawScreenPosition.y, 0, Screen.height);
            
         } 
-        else { behindCamera = false; }
-    
-        
-        
-        Indicator.position = renderPosition; 
+        else //if behind the camera: needs to always be clamped so it doesn't appear mirrored "in front"
+        { 
 
-        debugText.text = $"{Indicator.position.x}, {Indicator.position.y}, {Indicator.position.z}" + $"\n behind camera: {behindCamera}" + $"\n dist {distanceToMidpoint.x}";
-        
+            float xMidpoint = Screen.width / 2;
+            if (rawScreenPosition.x > xMidpoint) //if behind on the left side
+            {
+                finalScreenPosition.x = 0; //clamp to left side of screen
+            }
+            else
+            {
+                finalScreenPosition.x = Screen.width; //clamp to right side of screen
+            }
 
-        debugText.text = renderPosition.x.ToString() + " " + renderPosition.y.ToString();
+
+            float yMidpoint = Screen.height / 2;
+            if (rawScreenPosition.y > yMidpoint) // if behind on lower half
+            {
+                finalScreenPosition.y = 0; //clamp to bottom of screen
+            }
+            else
+            {
+                finalScreenPosition.y = Screen.height; //clamp to top of screen
+            }
+
+        }
+
+        GUIIndicator.position = finalScreenPosition; 
+
+
+        //SHOWCALL INPUT (temporary for now)
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            StartCoroutine(DisplayShowCallIndicator(pulseLength, fadeSpeed));
+        }
+
+
+
+        //DEBUG STUFF
+        string debug = rawScreenPosition.ToString();
+        debug += $"\n scale: {GUIIndicator.localScale.ToString()}";
+        debug += $"\nelapsed: {elapsed}";
+        debugText.text = debug;
         
     }
+
+
+    float elapsed = 0f;
+
+    IEnumerator DisplayShowCallIndicator (float pulseDuration, float fadeDuration) //coroutine to scale the GUI, keep it at 0 for normal
+    {
+        GUIIndicator.localScale = Vector3.one; //make indicator big & visible
+        yield return new WaitForSeconds(pulseDuration);  //keep it visible for [seconds]
+
+        //might add a fade-off here: 
+        //Vector3 startScale = GUIIndicator.localScale;
+        elapsed = 0f;
+
+        while (elapsed < fadeDuration) 
+        {
+            float t = elapsed / fadeDuration;
+            GUIIndicator.localScale = Vector3.Lerp(Vector3.one, Vector3.zero, t);
+            elapsed += Time.deltaTime;
+            yield return null;
+            
+        }
+
+        GUIIndicator.localScale = Vector3.zero;
+    }
 }
+
